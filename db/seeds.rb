a891efb6184bd5ee26278b78c5ff4c9a0f00c423
felipe <felipe.pinto.2912@gmail.com>
2022-10-06 22:00:34 -0300

    Corrections on seeds and database migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,45 +18,45 @@ airports = Airport.create!(
 
 airports.first.departing_flights.create!(
   [
-    { arrival_airport_id: airports[1], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 55 },
-    { arrival_airport_id: airports[2], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 105 },
-    { arrival_airport_id: airports[3], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 66 },
-    { arrival_airport_id: airports[4], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 115 }
+    { arrival_airport_id: airports[1].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 55 },
+    { arrival_airport_id: airports[2].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 105 },
+    { arrival_airport_id: airports[3].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 66 },
+    { arrival_airport_id: airports[4].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 115 }
   ]
 )
 
 airports[1].departing_flights.create!(
   [
-    { arrival_airport_id: airports[0], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 55 },
-    { arrival_airport_id: airports[2], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 100 },
-    { arrival_airport_id: airports[3], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 72 },
-    { arrival_airport_id: airports[4], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 104 }
+    { arrival_airport_id: airports[0].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 55 },
+    { arrival_airport_id: airports[2].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 100 },
+    { arrival_airport_id: airports[3].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 72 },
+    { arrival_airport_id: airports[4].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 104 }
   ]
 )
 
 airports[2].departing_flights.create!(
   [
-    { arrival_airport_id: airports[0], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 105 },
-    { arrival_airport_id: airports[1], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 100 },
-    { arrival_airport_id: airports[3], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 98 },
-    { arrival_airport_id: airports[4], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 165 }
+    { arrival_airport_id: airports[0].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 105 },
+    { arrival_airport_id: airports[1].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 100 },
+    { arrival_airport_id: airports[3].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 98 },
+    { arrival_airport_id: airports[4].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 165 }
   ]
 )
 
 airports[3].departing_flights.create!(
   [
-    { arrival_airport_id: airports[0], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 66 },
-    { arrival_airport_id: airports[1], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 72 },
-    { arrival_airport_id: airports[2], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 98 },
-    { arrival_airport_id: airports[4], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 95 }
+    { arrival_airport_id: airports[0].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 66 },
+    { arrival_airport_id: airports[1].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 72 },
+    { arrival_airport_id: airports[2].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 98 },
+    { arrival_airport_id: airports[4].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 95 }
   ]
 )
 
 airports.last.departing_flights.create!(
   [
-    { arrival_airport_id: airports[0], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 115 },
-    { arrival_airport_id: airports[1], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 104 },
-    { arrival_airport_id: airports[2], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 165 },
-    { arrival_airport_id: airports[3], date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 95 }
+    { arrival_airport_id: airports[0].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 115 },
+    { arrival_airport_id: airports[1].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 104 },
+    { arrival_airport_id: airports[2].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 165 },
+    { arrival_airport_id: airports[3].id, date: Time.new(2022, 10, 1) + 86_400 * rand(25..30), duration: 95 }
   ]
 )
