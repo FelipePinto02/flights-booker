4319cd8eb6e86003cd0d5a23508207eaf457e5a8
felipe <felipe.pinto.2912@gmail.com>
2022-10-06 22:52:08 -0300

    Added logic to show the search result

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,13 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.new
+    @flights = params[:flight] ? Flight.where(flight_search_params) : Flight.new
     @departure_options = Airport.all.map { |a| [a.code, a.id] }
     @arrival_options = Airport.all.map { |a| [a.code, a.id] }
     @date_options = Flight.order(:date).pluck(:date).uniq.map { |d| [d.strftime('%d/%m/%Y'), d] }
     @passengers_options = [['1', 1], ['2', 2], ['3', 3], ['4', 4]]
+  end
+
+  def flight_search_params
+    params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :date)
   end
 end
