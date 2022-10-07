676c68ea893d324c05bac9ce224a62536a3299c7
felipe <felipe.pinto.2912@gmail.com>
2022-10-07 15:06:38 -0300

    Generated and migrated the Booking model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,8 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: 'Airport'
   belongs_to :arrival_airport, class_name: 'Airport'
 
+  has_many :bookings
+
   def self.dates_options
     order(:date).pluck(:date).uniq.map { |d| [d.strftime('%d/%m/%Y'), d] }
   end
