ab9a44d43aa640600c7e8d700ffcbce2aa88ae29
felipe <felipe.pinto.2912@gmail.com>
2022-10-06 22:23:29 -0300

    Generated FlightsController and its #index action

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,9 @@
+class FlightsController < ApplicationController
+  def index
+    @flights = Flight.new
+    @departure_options = Airport.all.map { |a| [a.code, a.id] }
+    @arrival_options = Airport.all.map { |a| [a.code, a.id] }
+    @date_options = Flight.order(:date).pluck(:date).uniq.map { |d| [d, d] }
+    @passengers_options = [['1', 1], ['2', 2], ['3', 3], ['4', 4]]
+  end
+end
