c526b89bac1996c67e04526ff056cc7bfd7ed2d3
felipe <felipe.pinto.2912@gmail.com>
2022-10-06 22:27:40 -0300

    Changed how date appears on the index select dropdown

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,7 +3,7 @@ class FlightsController < ApplicationController
     @flights = Flight.new
     @departure_options = Airport.all.map { |a| [a.code, a.id] }
     @arrival_options = Airport.all.map { |a| [a.code, a.id] }
-    @date_options = Flight.order(:date).pluck(:date).uniq.map { |d| [d, d] }
+    @date_options = Flight.order(:date).pluck(:date).uniq.map { |d| [d.strftime('%d/%m/%Y'), d] }
     @passengers_options = [['1', 1], ['2', 2], ['3', 3], ['4', 4]]
   end
 end
