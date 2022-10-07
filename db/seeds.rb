6c057c7e1731304df3bf8128120d15fd6349c2df
felipe <felipe.pinto.2912@gmail.com>
2022-10-06 21:30:31 -0300

    Generated Airport model and added some airports to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+airports = Airport.create!(
+  [
+    { code: 'SDU' },
+    { code: 'GRU'},
+    { code: 'BSB'},
+    { code: 'VCP'},
+    { code: 'POA'}
+  ]
+)
